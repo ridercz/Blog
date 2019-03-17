9b7a76eca2a208207510b18c19af569bf92d0a98
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-03-17 17:50:20 +0100

    Using .NET Framework version of X4W locally

--- preview.cmd
+++ preview.cmd
@@ -1,7 +1,7 @@
 @ECHO off
 CLS
 
-dotnet run --framework netcoreapp2.1 --project ..\XML4web\Altairis.Xml4web.Compiler . -- .\build.json
+.\bin\x4w-compiler.exe .\x4w-build.json
 IF %ERRORLEVEL% NEQ 0 GOTO END
 
 dotnet serve -d docs --default-extensions:html -o
