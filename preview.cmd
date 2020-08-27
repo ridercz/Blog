d22fefa77172334c3165fa0448e5ebb5897122e9
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-08-27 18:23:27 +0200

    Update to dotnet tool version of XML4web

--- preview.cmd
+++ preview.cmd
@@ -1,9 +1,14 @@
 @ECHO off
 CLS
 
-.\bin\x4w-compiler.exe .\x4w-build.json
+REM -- Restore dotnet tools
+dotnet tool restore
+
+REM -- Compile site
+dotnet x4w-compiler x4w-build.json
 IF %ERRORLEVEL% NEQ 0 GOTO END
 
+REM -- Run local server
 dotnet serve -d docs --default-extensions:html -o
 
 :END
